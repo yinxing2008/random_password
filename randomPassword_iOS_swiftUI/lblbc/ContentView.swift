166821eba9cae9a8a4ee90a76fe64f6bd4436d00
jerry <309032663@qq.com>
2023-03-17 21:52:08 +0800

    update

--- randomPassword_iOS_swiftUI/lblbc/ContentView.swift
+++ randomPassword_iOS_swiftUI/lblbc/ContentView.swift
@@ -1,9 +1,8 @@
-//
-//  ContentView.swift
-//  lblbc
-//
-//  Created by jerry on 2023/3/14.
-//
+// 厦门大学计算机专业 | 前华为工程师
+// 专注《零基础学编程系列》  http://lblbc.cn/blog
+// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+// 公众号：蓝不蓝编程
+
 
 import SwiftUI
 import MobileCoreServices
