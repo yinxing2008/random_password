bca53bf5439ef0a8a05ba6b0d1c2be385fdf8065
jerry <309032663@qq.com>
2023-03-18 06:42:13 +0800

    update

--- randomPassword_android_kotlin/app/src/main/java/cn/lblbc/news/utils/Utils.kt
+++ randomPassword_android_kotlin/app/src/main/java/cn/lblbc/news/utils/Utils.kt
@@ -1,7 +1,12 @@
 package cn.lblbc.news.utils
 
 import kotlin.random.Random.Default.nextInt
-
+/**
+ * 厦门大学计算机专业 | 前华为工程师
+ * 专注《零基础学编程系列》  http://lblbc.cn/blog
+ * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+ * 公众号：蓝不蓝编程
+ */
 object Utils {
     private var availableChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()-+_=,."
 
